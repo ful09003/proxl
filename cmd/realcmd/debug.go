97e70e565523e88dd24a56e68b1b4237fd5e45a9
Michael Fuller <ful422.m@gmail.com>
2021-09-24 11:01:43 -0700

    add debug command

---
+++ cmd/realcmd/debug.go
@@ -0,0 +1,34 @@
+package realcmd
+
+import (
+	"os"
+
+	"github.com/ful09003/cards/internal"
+	expfmt "github.com/prometheus/common/expfmt"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(debugCmd)
+}
+
+var debugCmd = &cobra.Command{
+	Use: "debug",
+	Short: "Scrape Prometheus exporter",
+	Long: `
+Scrapes the desired Prometheus exporter target, and prints its output. Save yourself switching back to a browser!	
+`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		s := internal.NewCardsHttpScraper(target, 1)
+		mF, err := s.ScrapeTarget()
+		if err != nil {
+			return err
+		}
+
+		for _, actualFam := range mF {
+			expfmt.MetricFamilyToText(os.Stdout, actualFam)
+		}
+
+		return nil
+	},
+}
