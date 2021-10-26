5b0208efbadb395ae793ed33742f51b97f257db8
Michael Fuller <ful422.m@gmail.com>
2021-10-25 18:07:56 -0700

    Remove pre-major-imp code

--- internal/scorer.go
+++ internal/scorer.go
@@ -166,39 +166,3 @@ type CardsFamilyNameEvaluator struct {
 func (r *CardsFamilyNameEvaluator) Evaluate(f *dto.MetricFamily) (bool, error) {
 	return criteria.FamilyNameCheck(f, r.excludeList)
 }
-// CardsScoreFn is a function signature used to signal to a CardsScoringProcessor a pass/fail for the processor's purposes
-type CardsScoreFn func(mFam *dto.MetricFamily) (int, error)
-
-type CardsScoringModel struct {
-	criteria []CardsScoreFn
-	data     *dto.MetricFamily
-}
-
-// NewModel returns a Cards scoring model for the input MetricFamily.
-func NewModel(forFam *dto.MetricFamily) *CardsScoringModel {
-	return &CardsScoringModel{
-		criteria: []CardsScoreFn{},
-		data:     forFam,
-	}
-}
-
-// WithFns adds a variable number of functions to a Cards ScoringModel
-func (m *CardsScoringModel) WithFns(c ...CardsScoreFn) *CardsScoringModel {
-	m.criteria = append(m.criteria, c...)
-
-	return m
-}
-
-func (m *CardsScoringModel) Evaluate() (int, error) {
-	var score int
-
-	for _, f := range m.criteria {
-		evalScore, err := f(m.data)
-		if err != nil {
-			return score, err
-		}
-		score += evalScore
-	}
-
-	return score, nil
-}
